a766c5e4be19a6f29cd7f9e873d2071e4758f6fc
ShadowConYT <iamajay6122@gmail.com>
2023-06-28 10:41:28 +0530

    Started right side

---
+++ src/components/Cardcomp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cardcomp = () => {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Cardcomp
--- src/components/rightComp.js
+++ src/components/rightComp.js
@@ -14,14 +14,21 @@ const RightComp = () => {
                 <img width={'50px'} height={'50px'} style={{marginRight:'50px', borderRadius:'15px' }} src={Akaza} />
             </div>
         </div>
-        <div id='statistics' style={{backgroundColor:'white'}}>
-            <h1 style={{fontSize:'1em',marginBottom:'0px'}}>Upcoming Hours</h1>
-
-            <select name ='Rain Precipitation' id ='Rain Precipitation' class = 'Rain Precipitation' style = {{color: '#006699',fontFamily:'Arial',fontSize: '12px',fontStyle: 'normal',backgroundColor: '#FFFFFF',padding: "2px"}}>
-            <option value = "#">Rain Precipitation 1</option>
-            <option value = "#">Rain Precipitation 2</option>
-            </select>
-
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <div id='statistics' style={{backgroundColor:'white',width:'90%',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+                <h1 style={{fontSize:'1em',marginBottom:'0px',marginTop:'2%',marginLeft:'3%'}}>Upcoming Hours</h1>
+                <div style={{marginTop:'2%',marginLeft:'3%'}}>
+                    <select name ='Rain Precipitation' id ='Rain Precipitation' class = 'Rain Precipitation' style = {{color: '#a7a7a7',fontFamily:'Arial',fontSize: '12px',marginRight:'25px',fontStyle: 'normal',border:'2px solid #f1f1f1',borderRadius:'5px',backgroundColor: '#F1f1f1',padding: "2px"}}>
+                    <option value = "#">Rain Precipitation 1</option>
+                    <option value = "#">Rain Precipitation 2</option>
+                    </select>
+                    
+                    <select name ='Next Day' id ='Next Day' class = 'Next Day' style = {{color: '#000000',marginRight:'30px',fontFamily:'Arial',fontSize: '12px',fontStyle: 'normal',backgroundColor: '#F1f1f1',border:'2px solid #f1f1f1',borderRadius:'5px',padding: "2px"}}>
+                    <option value = "#">Next Day</option>
+                    <option value = "#">Yesterday</option>
+                    </select>
+                </div>
+            </div>
         </div>
     </div>
   )
